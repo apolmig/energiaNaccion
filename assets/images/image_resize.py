452e9d031f5a151f60661c215d18943bdc898015
Miguel <apolmig@gmail.com>
2018-01-23 17:22:48 +0100

    Add files via upload

---
+++ assets/images/image_resize.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+from PIL import Image
+import os, sys
+
+path = os.getcwd()
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((200,150), Image.ANTIALIAS)
+            imResize.save(f + ' resized.jpg', 'JPEG', quality=90)
+
+resize()
